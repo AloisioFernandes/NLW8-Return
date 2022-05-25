ad756ca9e51a94b6762e023482a395402baf35aa
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-05-24 23:04:39 -0300

    Tailwind.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <h1>Hello World</h1>
+  return <h1 className="bg-violet-500 px-4 h-10 rounded text-violet-100 hover:bg-violet-700">Hello World</h1>
 }
 
 export default App
