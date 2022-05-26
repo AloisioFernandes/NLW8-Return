1ff51b29393ceaf0af6dd97007916b583c5b781b
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-05-25 23:43:44 -0300

    Iniciando widget.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,5 @@
-function App() {
-  return <h1 className="bg-violet-500 px-4 h-10 rounded text-violet-100 hover:bg-violet-700">Hello World</h1>
-}
+import { Widget } from "./components/Widget"
 
-export default App
+export function App() {
+  return <Widget />
+}
