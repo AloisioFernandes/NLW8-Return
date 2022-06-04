310deebf48e657344098e69416aaebfcd42e1d69
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-06-04 16:37:21 -0300

    Iniciando footer e header.

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,7 +1,15 @@
 export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      Hello World
+      <header>
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+      </header>
+
+      <p>Hello World</p>
+
+      <footer className="test-xs text-neutral-400">
+        Feito com ♥ por <a className="underline" href="https://rocketseat.com.br">Rocketseat</a>
+      </footer>
     </div>
   )
 }
