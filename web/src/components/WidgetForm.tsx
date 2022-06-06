03107c40c0e80106e00417010f7bedb54a3a2c84
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-06-05 21:20:53 -0300

    Botão X.

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,14 +1,18 @@
+import { CloseButton } from "./CloseButton";
+
 export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
         <span className="text-xl leading-6">Deixe seu feedback</span>
+
+        <CloseButton />
       </header>
 
       <p>Hello World</p>
 
       <footer className="test-xs text-neutral-400">
-        Feito com ♥ por <a className="underline" href="https://rocketseat.com.br">Rocketseat</a>
+        Feito com ♥ por <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
       </footer>
     </div>
   )
