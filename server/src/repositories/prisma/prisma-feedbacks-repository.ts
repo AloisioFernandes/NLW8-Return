8e2006155426a3e16729aa1242cef29be94f3e93
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-07-03 22:31:40 -0300

    Iniciando casos de uso.

--- server/src/repositories/prisma/prisma-feedbacks-repository.ts
+++ server/src/repositories/prisma/prisma-feedbacks-repository.ts
@@ -1,5 +1,14 @@
-import { FeedbacksRepository } from "../feedbacks-respository";
+import { prisma } from "../../prisma";
+import { FeedbackCreateData, FeedbacksRepository } from "../feedbacks-respository";
 
 export class PrismaFeedbacksRepository implements FeedbacksRepository {
-  
+  async create({ type, comment, screenshot  }: FeedbackCreateData) {
+    await prisma.feedback.create({
+      data: {
+        type,
+        comment,
+        screenshot
+      }
+    })
+  }
 }
