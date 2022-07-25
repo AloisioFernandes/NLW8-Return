06dbeb9b042eed49f9e5681dc061a6bab4c98cc1
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-07-25 19:39:21 -0300

    Estilos componente form.

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -5,6 +5,7 @@ import BottomSheet from "@gorhom/bottom-sheet";
 import { gestureHandlerRootHOC } from "react-native-gesture-handler";
 
 import { Options } from "../Options";
+import { Form } from "../Form";
 
 import { styles } from "./styles";
 
@@ -37,7 +38,7 @@ function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-        <Options />
+        <Form feedbackType="BUG" />
       </BottomSheet>
     </>
   );
